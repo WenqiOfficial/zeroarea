15a597ff03a9a4bcd63d9291cbbd9573c90f3294
WenqiOfficial <2747297361@qq.com>
2022-04-16 16:17:06 +0800

    Create ico.js

---
+++ ico.js
@@ -0,0 +1,17 @@
+function change_icon() {
+            const changeFavicon = link => {
+                let $favicon = document.querySelector('link[rel="icon"]');
+                if ($favicon !== null) {
+                    $favicon.href = link;
+                } else {
+                    $favicon = document.createElement("link");
+                    $favicon.rel = "icon";
+                    $favicon.href = link;
+                    document.head.appendChild($favicon);
+                }
+            };
+            // 设置图标地址
+            let iconUrl = `//tablad.ml/favicon.ico`
+            // 动态修改网站图标
+            changeFavicon(iconUrl); 
+        }

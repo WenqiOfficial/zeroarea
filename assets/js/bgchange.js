a02d41a1aed426fd3142cb93f7db7b9fbc110910
WenqiOfficial <2747297361@qq.com>
2022-05-21 16:11:57 +0800

    新增 新的背景图

--- assets/js/bgchange.js
+++ assets/js/bgchange.js
@@ -10,13 +10,25 @@ $(function () {
         
     //自定义图片↓
     pic[0]="https://cloud.zeroarea.ml:5221/api/v3/file/get/407/pic02new.webp?sign=oEJPS0C75qX5Ox1mQ7ZfIAV7XeN_iyCwUDR8VZv3kN8=:0";
-    picpid[0]= 79776133
+    picpid[0]= 79776133;
     
     pic[1]="https://cloud.zeroarea.ml:5221/api/v3/file/get/552/pic01new.webp?sign=B1iWbDd580RSTiBS23D1QNsEc-fIvUbFVOvCEhUo-yc%3D%3A0";
-    picpid[1]= 83508760
+    picpid[1]= 83508760;
     
     pic[2]="https://cloud.zeroarea.ml:5221/api/v3/file/get/553/pic03new.webp?sign=-6tPKe7D5bo6Q4FA4q-if_mTwdDbJPvbo8Q7J1mnoIc%3D%3A0";
-    picpid[2]= 76685776
+    picpid[2]= 76685776;
+
+    pic[3]="https://cloud.zeroarea.ml:5221/api/v3/file/get/957/pic04new.webp?sign=y9Ue5XevB2gCYf1JNH_8tbO3lcX3BnDJ4eCaURRHqZg%3D%3A0";
+    picpid[3]= 75778903;
+
+    pic[4]="https://cloud.zeroarea.ml:5221/api/v3/file/get/966/pic05new.webp?sign=4l0xy5xmC9138kN7Y1jXvlyjgru6RuaGfmxpuJtyY1Q%3D%3A0";
+    picpid[4]= 51698567;
+
+    pic[5]="https://cloud.zeroarea.ml:5221/api/v3/file/get/962/pic06new.webp?sign=TG858chNCBA_RPFrGm29XgY_4JtljZHobSUyVHBzhwA%3D%3A0";
+    picpid[5]= 34844544;
+
+    pic[6]="https://cloud.zeroarea.ml:5221/api/v3/file/get/964/pic07new.webp?sign=ew-wFvqpEGoJY43e6eyyOQs0t34khCCKbNFUYS--Lso%3D%3A0";
+    picpid[6]= 88330224;
 
     $window.on('load', function() {
         //切换间隔时间（毫秒）

5008dcb2a3650992c706f9e8527e0e2bb099d6ed
WenqiOfficial <2747297361@qq.com>
2023-07-19 15:31:37 +0800

    Update live-dplayer.js

--- streampage/live-dplayer.js
+++ streampage/live-dplayer.js
@@ -7,7 +7,7 @@ liveroom[3]='game3';
 
 function device(){
     var md = new MobileDetect(window.navigator.userAgent);
-    if(md.is('Safari') || md.is('iPhone') || md.is('iPad') || md.is('Macintosh'))
+    if(md.is('Safari') || md.is('iPhone') || md.is('iPad') || md.is('Mac OS'))
     {
         videotype='m3u8';
         $.NZ_MsgBox.tipsbar({

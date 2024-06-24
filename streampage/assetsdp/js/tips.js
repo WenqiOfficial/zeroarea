353a632e9ac213147a83a88ed0e137c4c8a5d90b
WenqiOfficial <2747297361@qq.com>
2024-06-24 15:26:58 +0800

    new fuction

--- streampage/assetsdp/js/tips.js
+++ streampage/assetsdp/js/tips.js
@@ -3,55 +3,55 @@ var botton = $("#testbotton"),
     date = new Date(),
     shutdown = 0;
 
-$window.on('load', function() {
+$window.on('load', function () {
     window.setTimeout(firsttips, 1000);
 });
 
-function testserver(){
-//检测服务器是否正常
-var img = new Image();
-img.src = "https://stream.wenqi.icu:48088/favicon.ico";
+function testserver() {
+    //检测服务器是否正常
+    var img = new Image();
+    img.src = "https://stream.wenqi.icu:48088/favicon.ico";
 
-var timeout = setTimeout(function () {
-    img.onerror = img.onload = null;
-    console.log("server time out.");
-    canNotLoad();
-},3000);
+    var timeout = setTimeout(function () {
+        img.onerror = img.onload = null;
+        console.log("server time out.");
+        canNotLoad();
+    }, 3000);
 
-img.onerror = function () {
-    clearTimeout(timeout);
-    console.log("server load error.");
-    shutdown = 1;
-    canNotLoad();
-}
+    img.onerror = function () {
+        clearTimeout(timeout);
+        console.log("server load error.");
+        shutdown = 1;
+        canNotLoad();
+    };
 
-img.onload = function () {
-    clearTimeout(timeout);
-    console.log("server ok.");
-    canLoad();
-}
+    img.onload = function () {
+        clearTimeout(timeout);
+        console.log("server ok.");
+        canLoad();
+    };
 
-function canNotLoad() {
-    var msgtext="与服务器的连接超时！可能会加载失败！"
-    if(shutdown==1) msgtext="与服务器的连接错误！"
-    $.NZ_MsgBox.tipsbar({
-        title: "哔哔哔！",
-        content: msgtext,
-        type: "warning",
-        showtime: 5000,
-        processbar: false
-    });
-}
-function canLoad() {
-    $.NZ_MsgBox.tipsbar({ 
-        title: "ヾ(•ω•`)o"
-        , content: '与服务器的连接正常！'
-        , type: "success"
-        , showtime: 3000
-        , processbar: false
-    });
-}
-//
+    function canNotLoad() {
+        var msgtext = "与服务器的连接超时！可能会加载失败！";
+        if (shutdown == 1) msgtext = "与服务器的连接错误！";
+        $.NZ_MsgBox.tipsbar({
+            title: "哔哔哔！",
+            content: msgtext,
+            type: "warning",
+            showtime: 5000,
+            processbar: false
+        });
+    }
+    function canLoad() {
+        $.NZ_MsgBox.tipsbar({
+            title: "ヾ(•ω•`)o"
+            , content: '与服务器的连接正常！'
+            , type: "success"
+            , showtime: 3000
+            , processbar: false
+        });
+    }
+    //
 }
 
 function firsttips() {
@@ -85,7 +85,7 @@ $.NZ_MsgBox.tooltip({
     location: "bottom"
 });
 $.NZ_MsgBox.tooltip({
-    tiptarget: $("#E"),
-    content: "房间：⌈ 电动组4 ⌋",
+    tiptarget: $("#custom"),
+    content: "自定义房间号",
     location: "bottom"
 });
--- streampage/live-dplayer.js
+++ streampage/live-dplayer.js
@@ -1,16 +1,15 @@
 var liveroom = new Array,
-    videotype='flv';
-liveroom[0]='livetest';
-liveroom[1]='game1';
-liveroom[2]='game2';
-liveroom[3]='game3';
+    videotype = 'flv';
+liveroom[0] = 'livetest';
+liveroom[1] = 'game1';
+liveroom[2] = 'game2';
+liveroom[3] = 'game3';
 
-function device(){
+function device() {
     let uap = new UAParser(window.navigator.userAgent);
     var md = new MobileDetect(window.navigator.userAgent);
-    if( md.is('Safari') || md.is('iPhone') || md.is('iPad') || uap.getDevice().is("Apple") )
-    {
-        videotype='m3u8';
+    if (md.is('Safari') || md.is('iPhone') || md.is('iPad') || uap.getDevice().is("Apple")) {
+        videotype = 'm3u8';
         $.NZ_MsgBox.tipsbar({
             title: "检测到非 FLV 支持设备",
             content: "已自动切换到 HLS 格式了！",
@@ -21,35 +20,58 @@ function device(){
     }
 }
 
-function liveplay(id){
+function liveplay(id) {
     // setTimeout(function(){
     //     // alert("firstplay"+id);
     //     $('#wrapper').addClass('playing');
     //     $('#l'+id).append("<p class='playerbox'><iframe allowfullscreen='allowfullscreen' mozallowfullscreen='mozallowfullscreen' msallowfullscreen='msallowfullscreen' oallowfullscreen='oallowfullscreen' webkitallowfullscreen='webkitallowfullscreen' class='liveplayer' width='100%' height='100%' scrolling='no' frameborder='0' frameborder src='liveroom/index-dplayer.html?room="+liveroom[String.fromCharCode(id.charCodeAt()-17)]+"'></iframe></p>");
     // },100)
-    $('#l'+id).append("<div id='dplayer'></div>");
-    creatplayer(liveroom[String.fromCharCode(id.charCodeAt()-17)]);
+    $('#l' + id).append("<div id='dplayer'></div>");
+    creatplayer(liveroom[String.fromCharCode(id.charCodeAt() - 17)]);
 }
 
-function livestop()
-{
+function liveplaycustom() {
+    $.NZ_MsgBox.prompt({
+        title: "指定房间名：", type: "warning", callback: function (resu) {
+            if (resu === '') {
+                $.NZ_MsgBox.alert({
+                    title: "咦，似乎没有输入？"
+                    , content: "请输入正确的房间号！"
+                    , type: "warning"
+                });
+                liveplaycustom();
+            } else if (resu !== null) {
+                $.NZ_MsgBox.tipsbar({
+                    title: "哔哔哔！", content: '正在尝试连接：' + resu + ' ......', type: "info", showtime: 5000
+                });
+                $('#lcustom').append("<div id='dplayer'></div>");
+                creatplayer(resu);
+                window.location.href = "#lcustom";
+            }
+        }
+    });
+}
+
+function livestop() {
     // if($('#wrapper').hasClass('playing')) {
-        // alert("stop");
-        setTimeout(function(){
-            $('#dplayer').remove();
-        },500);
-        // $('#wrapper').removeClass('playing');}
+    // alert("stop");
+    setTimeout(function () {
+        $('#dplayer').remove();
+    }, 500);
+    // $('#wrapper').removeClass('playing');}
 }
 
 device();
-$('.close').on('click',livestop);
+$('.close').on('click', livestop);
 // $('#wrapper').on('click',livestop);
-if(window.location.hash!=''){
+if (window.location.hash == '#lcustom') {
+    liveplaycustom();
+} else if (window.location.hash != '') {
     // alert(window.location.hash.substr(1));
     liveplay(window.location.hash.substr(2));
 }
 
-function creatplayer(room){
+function creatplayer(room) {
     var fullscreen = 0;
     const dp = new DPlayer({
         container: document.getElementById('dplayer'),
@@ -68,20 +90,20 @@ function creatplayer(room){
             },
         },
         video: {
-            url: 'https://stream.wenqi.icu:48088/live/'+room+'.'+videotype,
+            url: 'https://stream.wenqi.icu:48088/live/' + room + '.' + videotype,
             type: videotype,
         },
     });
-    dp.on('fullscreen', function(){fullscreen=1;});
-    dp.on('fullscreen_cancel', function(){fullscreen=0;});
+    dp.on('fullscreen', function () { fullscreen = 1; });
+    dp.on('fullscreen_cancel', function () { fullscreen = 0; });
     dp.on('resize', function () {
         console.log('remove set!');
-        setTimeout(function(){
-            if($('#dplayer').hasClass('dplayer-fulled')){
-                $('.main').attr('id','');
-            }else if(fullscreen==0){
-                $('.main').attr('id','main');
+        setTimeout(function () {
+            if ($('#dplayer').hasClass('dplayer-fulled')) {
+                $('.main').attr('id', '');
+            } else if (fullscreen == 0) {
+                $('.main').attr('id', 'main');
             }
-        },10);
+        }, 10);
     });
 }
